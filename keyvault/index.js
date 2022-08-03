2aa7bbdd1b7b52f7f7cdb781cdc76c9fbb8e07dd
Han Ho <hstdra@gmail.com>
2022-08-04 00:23:41 +0700

    Update code

--- keyvault/index.js
+++ keyvault/index.js
@@ -1,9 +1,9 @@
-const { DefaultAzureCredential } = require("@azure/identity");
+const { ManagedIdentityCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 
-const credential = new DefaultAzureCredential();
+const credential = new ManagedIdentityCredential("50ae3701-8e7a-421f-8f79-dbce496fa64c");
 
-const vaultName = "tpick";
+const vaultName = "tpick-vault";
 const url = `https://${vaultName}.vault.azure.net`;
 
 const client = new SecretClient(url, credential);
@@ -11,9 +11,9 @@ const client = new SecretClient(url, credential);
 const secretName = "test";
 
 async function main() {
-  console.log("Welcome to key")
+  console.log("Welcome to key");
   const secret = await client.getSecret(secretName);
-  console.log("Secret value:", secret.value)
+  console.log("Secret value:", secret.value);
 }
 
 main();
